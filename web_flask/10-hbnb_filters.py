3c6dbab16d7a5ccb9d5c9548ff4b278e54310315
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2024-03-12 23:15:03 -0500

    build

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,1 +1,15 @@
+#!/usr/bin/python3
+"""Module for starting a Flask web application"""
 
+import sys
+from flask import Flask
+from flask import render_template
+from models.state import State
+from models.city import City
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    storage.close()
