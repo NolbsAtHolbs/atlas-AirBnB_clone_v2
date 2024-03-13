c3560268ae6799a53f279d616243087a3b2bda01
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2024-03-12 22:20:41 -0500

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python3
 """Holds class State"""
 
-import models
-from models import storage
-from models.base_model import BaseModel, Base
-from models.city import City
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
 from models.base_model import BaseModel, Base
+from models import storage
 
 
 class State(BaseModel, Base):
-    """Representation of state"""
+    """ State class """
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
 
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City", backref="state", cascade="all, delete-orphan")
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state", cascade="all, delete")
     else:
+        name = ""
+
         @property
         def cities(self):
-            """City property for state reference"""
             city_all = []
-            for city_ex in models.storage.all(City).values():
-                if city_ex.state_id == self.id:
-                    city_all.append(city_ex)
+            for key in storage.all("City"):
+                if storage.all("City")[key].state_id == self.id:
+                    city_all.append(storage.all("City")[key])
             return city_all
